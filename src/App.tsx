5ae33dfb6937e210219e61d9dc1b3d01910ce829
Samuel Petersson <samuel.petersson@hotmail.com>
2026-07-03 23:56:42 +0200

    Tillagd fetch och hämtning av todos samt types

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,48 @@
-import './App.css'
+import './App.css';
+import { useState, useEffect } from 'react';
+import type { Todo } from './types';
 
 function App() {
-  
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState('');
+
+  const getTodos = async () => {
+    try {
+      const res = await fetch('http://localhost:5000/api/todos');
+
+      if (!res.ok) {
+        throw new Error('Kunde inte hämta todos');
+      }
+
+      const data = await res.json();
+      setTodos(data);
+    } catch (err) {
+      setError('Något gick fel vid hämtning av todos');
+      console.error(err);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    getTodos();
+  }, []);
+
+  if (loading) return <p>Laddar...</p>;
+  if (error) return <p>{error}</p>;
+
+  return (
+    <div>
+      <h1>Att göra lista</h1>
+      <ul>
+        {todos.map((todo) =>(
+          <li key={todo._id}>
+            {todo.title} - {todo.status}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 export default App
---
+++ src/types.ts
@@ -0,0 +1,8 @@
+export interface Todo {
+    _id: string;
+    title: string;
+    description: string;
+    status: string;
+    createdAt: string;
+    updatedAt: string;
+}
